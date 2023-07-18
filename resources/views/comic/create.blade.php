1b3bb1b9450b0e3ddfd8063542c366409d2f466a
FRAALES <alessandrofranco1992@gmail.com>
2023-07-18 17:46:41 +0200

    create quasi finito

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -1,28 +1,47 @@
 @extends("layouts.main-layout")
 
-@section("section")
+@section("content")
     <div class="text-center">
         <h1>
             New Comic
         </h1>
         <form>
-            <label for=""></label>
-            <input type="text">
+            <label for="title">Title</label>
+            <br>
+            <input type="text" name="title">
+            <br>
 
-            <label for=""></label>
-            <input type="text">
+            <label for="description">Description</label>
+            <br>
+            <input type="text" name="description">
+            <br>
             
-            <label for=""></label>
-            <input type="text">
+            <label for="price">Price</label>
+            <br>
+            <input type="text" name="price">
+            <br>
 
-            <label for=""></label>
-            <input type="text">
+            <label for="series">Series</label>
+            <br>
+            <input type="text" name="series">
+            <br>
 
-            <label for=""></label>
-            <input type="text">
+            <label for="sale_date">Sale</label>
+            <br>
+            <input type="text" name="sale_date">
+            <br>
 
-            <label for=""></label>
-            <input type="text">
+            <label for="type">Type</label>
+            <br>
+            <input type="text" name="type">
+            <br>
+            <br>
+            <br>
+
+            <input type="submit" value="CREATE">
+            <br>
+            <br>
+            <br>
 
         </form>
     </div>
