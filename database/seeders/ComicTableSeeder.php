25ab175c4c7c1c65278d09f33ecf3ad91494f7d3
FRAALES <alessandrofranco1992@gmail.com>
2023-07-18 15:36:45 +0200

    comic instead of comicmodel

--- database/seeders/ComicTableSeeder.php
+++ database/seeders/ComicTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Comic;
+
 class ComicTableSeeder extends Seeder
 {
     /**
@@ -14,7 +16,7 @@ class ComicTableSeeder extends Seeder
      */
     public function run()
     {
-        $comics = [
+        $data = [
             [
                 "title" => "Action Comics #1000: The Deluxe Edition",
                 "description" => "The celebration of 1,000 issues of Action Comics continues with a new, Deluxe Edition of the amazing comic that won raves when it hit comics shops in April! This hardcover includes all the stories from that issue, plus the tale by writer Paul Levitz and artist Neal Adams that appeared in the Action Comics: 80 Years Of Superman hardcover, as well as all the variant covers, design sketches by Jim Lee for Superman’s new look, scripts for the stories, the original art from the lost story featuring art by Curt Swan and more! Plus: a complete reprint of the stories that started it all—the Superman stories Action Comics #1 and 2 from 1938!",
@@ -230,7 +232,7 @@ class ComicTableSeeder extends Seeder
 
         foreach ($data as $comicData) {
 
-            Comic :: create([
+            $comic = Comic :: create([
                 "title" => $comicData["title"],
                 "description" => $comicData["description"],
                 "price" => $comicData["price"],
